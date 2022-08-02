c934e54cd8e4f6725361fefaecd890996ec5f61d
skWeenggs <104074453+skWeenggs@users.noreply.github.com>
2022-08-02 17:00:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const { MongoTopologyClosedError } = require("mongodb");
 
 const mongoose = require("mongoose");
 const User = require("./model/Post");
-// const userRouter= require("./Routes/Routes")
+
 const cors=require('cors');
 require("dotenv").config();
 
